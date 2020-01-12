30932e03e4b96e097908627a93953252526c9961
Victor Li <victorli@victors-mbp.attlocal.net>
2020-01-11 23:52:43 -0800

    started first example of Super()

---
+++ Excercise_Super().py
@@ -0,0 +1,30 @@
+# This will be quick exercise with the Super() class function.
+# Using super() in Single Inheritance
+
+
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return self.length * 2 + self.width * 2
+
+
+rectangle = Rectangle(3, 2)
+print(f'The area of rectangle is: {rectangle.area()}')
+print(f'The Perimeter of rectangle is: {rectangle.perimeter()}')
+
+
+class Square(Rectangle):
+    # using the super() we declare that Square inherits from Rectangle Class.
+    def __init__(self, length):
+        super().__init__(length, length)
+
+
+square = Square(3)
+print(f'The area of square is: {square.area()}')
+print(f'The perimeter of square is: {square.perimeter()}')
