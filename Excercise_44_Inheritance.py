05ef2c1ab0bf768dd1c6d6905aefe5dd3f7105c2
Victorli888 <victorli7462@gmail.com>
2020-01-13 16:41:40 -0800

    Lesson 44 complete review needed

--- Excercise_44_Inheritance.py
+++ Excercise_44_Inheritance.py
@@ -1,4 +1,4 @@
-# Demonstrating Implicit Inheritance
+# Demonstrating Implicit Inheritance (Is-a) A house is-a Building
 class Parent(object):
 
     def implicit(self):
@@ -13,7 +13,7 @@ son = Child()
 
 dad.implicit()
 son.implicit() #  When called it instead calls upon its parent class.
-
+print("\n")
 # Demonstrating Override Explicitly
 class Parent1(object):
 
@@ -42,7 +42,7 @@ son.overide()
 son.inher()  # Inherit from Parent class
 daughter.inher()  # Inherit from same parent class
 son_of_son.inher() # Double inheritance?
-
+print("\n")
 # Demonstrate Altering before or after
 
 class Parent2(object):
@@ -55,16 +55,16 @@ class Child2(Parent2):
 
     def altered(self):
         print("Child Before Parent altered()")
-        super(Child2,self).altered()
+        super(Child2, self).altered()
         print("Child, After Parent altered()")
 
-super()
+
 dad = Parent2()
 son = Child2()
 
 dad.altered()
 son.altered()
-
+print("\n")
 # Demonstrate all 3 of these examples.
 
 class Parent3(object):
@@ -88,3 +88,52 @@ class Child3(Parent3):
         super(Child3, self).altered()
         print("Child, After Parent altered().")
 
+
+dad = Parent3()
+son = Child3()
+
+dad.implicit()
+son.implicit()
+
+dad.override()
+son.override()
+
+dad.altered()
+son.altered()
+print("\n")
+
+# We can do the all of the above with Composition as well. ( Has-a)
+# Quick review of Has-a Is-a  .... A House Is-a Building .... A house has-a Room
+
+class Other4(object):
+    def override(self):
+        print("OTHER override()")
+
+    def implicit(self):
+        print("OTHER implicit()")
+
+    def altered(self):
+        print("OTHER altered()")
+
+
+class Child4(object):
+
+    def __init__(self):
+        self.other = Other4()
+
+    def implicit(self):
+        self.other.implicit()
+
+    def override(self):
+        print("CHILD override()")
+
+    def altered(self):
+       print("CHILD, BEFORE OTHER altered()")
+       self.other.altered()
+       print("CHILD, AFTER OTHER altered")
+
+son = Child4()
+
+son.implicit()
+son.override()
+son.altered()
