b3cd1bb47d24f663dca55ef49ac5af04f0469841
Victor Li <victorli@victors-mbp.attlocal.net>
2020-01-26 20:00:32 -0800

    Finished

--- recursion_example_1.py
+++ recursion_example_1.py
@@ -80,4 +80,43 @@ def recursive_sum(number):
 
 recursive_sum(1)
 
-print("Example 4:")
+print("\nExample 4: List Recursion")
+
+def list_sum_recursion(input_list):
+    if input_list == []:  # this is the base case
+        return 0
+
+    else:  # Recursive Case ( We made a smaller version of it self with recursion)
+        head = input_list[0]
+        cut_list = input_list[1:]  # Cuts from the 1st Cardinal (Counting) position and beyond
+        return head + list_sum_recursion(cut_list)  # "Return the head of the list, and cut it down again"
+
+
+list = [5, 2, 3, 2, 1, 3]
+new_list = list[3:]  # This should return [2, 1, 3]
+new_list2 = list[5:]  # This would return [3]
+print(new_list)
+print(new_list2)
+print("Now lets move onto the recursive example")
+print(f"If I added all these numbers {list} they would add up to {list_sum_recursion(list)}")
+
+print("\nExample 5: Fibonacci's numbers Feat lru_cache")
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+# lru_cache is a decorator that caches results, This helps with the unnecessary re-computation of f(n) by checking the
+# value before trying to compute it.
+
+def fibonacci_recursive(n):
+    print(f"Calculating f({n}) ", end=",")
+    #  Base Case
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+
+    # Recursive Case
+    else:
+        return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+# fibonacci_recursive(n-2) causes the program to repeat But I don't have the definte answer to why.
+fibonacci_recursive(5)
