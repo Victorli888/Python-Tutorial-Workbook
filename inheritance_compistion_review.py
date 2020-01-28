69cb0017f5b9b658d1b55152458dc6e6626e47fc
Victor Li <victorli@victors-mbp.attlocal.net>
2020-01-27 20:05:24 -0800

    added Salary, Hourly, & Commission Employee classes

--- inheritance_compistion_review.py
+++ inheritance_compistion_review.py
@@ -5,9 +5,15 @@
 # We're going to do an example where HR system needs to process payroll for the company’s employees,
 # but there are different types of employees depending on how their payroll is calculated.
 
+class Employee:    # This is Base class that all the other classes will refrence to
+    #  For example, All employee have an ID and Name (Has-A)
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
 
-class Payroll_system:
-    def calculate_payroll(self,employees):
+
+class PayrollSystem:
+    def calculate_payroll(self, employees):
         print("Calculating Payroll...")
         print("=====================")
         for employee in employees:
@@ -15,33 +21,29 @@ class Payroll_system:
             print(f"Is {employee.calculate_payroll()}")
 
 
-class Employee:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-
-
 class SalaryEmployee(Employee):
     def __init__(self, id, name, weekly_salary):
-        super().__init__(id, name)
+        super().__init__(id, name) # Has-A id and Name
         self.weekly_salary = weekly_salary
 
     def calculate_payroll(self):
-        return.self.weekly_salary
+        return self.weekly_salary
 
-class HourlyEmployee(Employee):
-    def __init__(self, id, name, hourly_wage)
-        super().__init__(self, id, name)
+class HourlyEmployee(Employee):  # Is-A Employee
+    def __init__(self, id, name, hourly_wage):
+        super().__init__(id, name)  # has an ID and Name
         self.hourly_wage = hourly_wage
 
     def calculate_payroll(self):
         return self.hourly_wage
 
 
-class CommissionedEmployee(SalaryEmployee):
-    def __init__(self, id, name, weekly_salary, commission)
-        super().__init__(self,id,name,weekly_salary)
+class CommissionedEmployee(SalaryEmployee):  # Is-A SalaryEmployee
+    def __init__(self, id, name, weekly_salary, commission):
+        super().__init__(id, name, weekly_salary)  # Has-A ID, Name, and Weekly_Salary
         self.commission = commission
 
     def calculate_payroll(self):
-        fixed_salary = super().calculate_payroll(Sal
+        fixed_salary = super().calculate_payroll()
+        # Has-A relationship with SalaryEmployee in terms of calculate.payroll
+        return fixed_salary + self.commission
