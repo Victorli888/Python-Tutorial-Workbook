1ed9154b6c01c198024078d213a045192c7df500
Victor Li <victorli@victors-mbp.attlocal.net>
2020-02-07 08:32:24 -0800

    finished exercise

--- 6_Ternary Operators.py
+++ 6_Ternary Operators.py
@@ -15,3 +15,19 @@ personality = ("not nice", "nice")[nice]
 print(f"The cat is {personality}")
 
 # Avoid this because both elements of true and false is being evaluated (takes up extra memory)
+# Using the if-else ternary is better for this reason
+
+print("\nExample 3: If-Else Ternary")
+condition = True
+print("its True" if condition else "super not true")
+print(("its True", "Not True")[condition])
+
+# As you can tell the if-else ternary is much clearer in describing what is the False or True Condition
+
+print("\nExample4: ShortHand Ternary")
+True or "some"
+# this returns True
+False or "some"
+# this returns some
+
+# usze this to quickly check the out put of a function and give it a useful message if the output is empty.
