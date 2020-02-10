3803fae32761603e4ff45cf574123f49b63d8203
Victor Li <victorli@victors-mbp.attlocal.net>
2020-02-10 00:08:04 -0800

    added new lessons

--- 7_Decorators.py
+++ 7_Decorators.py
@@ -1,5 +1,6 @@
 # Decorators are... functions which modify the functionality of other functions, Making them shorter and more pythonic
 # The following examples are used to help make your code alot more concise
+# We use decorators to execute code before and after a function
 
 print("Example 1: Understanding functions in python")
 
@@ -44,3 +45,71 @@ hey()
 # are not available outside the hi() function
 
 # greet_2()  # this will output: NameError: name 'greet' is not defined
+print("\nExample 3: Returning functions from within functions")
+
+
+def hi3(name="superman"):
+    def greet3():
+        return "now you are in the greet3 function"
+
+    def welcome3():
+        return "now you are in the welcome3 function"
+
+    if name == "superman":
+        return greet3
+    else:
+        return welcome3  # we don't add the () at the ends of these because ()-- at the end executes the function
+    # if we leave it off we're able to pass them around and assign them to other variables without executing them
+
+
+a = hi3()  # by default, hi3() = hi3(name="superman") and thus it enters the hi3 function, but without another pair of
+# (parenthesis) then the we're not able to execute the functions in the function
+# by changing hi3() to hi3(name="regularman") then we return our else statement or welcome3
+
+print(a)  # this will output  <function greet at 0x7f213c01500 --- This is why this "error" message displays
+print(a())  # this will print the expected outcome
+
+
+print("\nExample 4: Giving a function as an argument to another function:")
+#  we can use the argument of a function to call on another function
+
+def hi4():
+    return "General Kenobi, YOU ARE A BOLD ONE."
+
+def doSomethingBeforeHi(func):
+    print("Why, Hello there (͡° ͜ʖ ͡°)")
+    print(func)
+
+
+doSomethingBeforeHi(hi4())
+
+print("\nExample 5: writing a decorator")
+
+def decorator4(a_func):
+
+    def wrapTheFunction():
+        print("I am doing some boring work before executing a_func()")
+
+        a_func()
+
+        print("I'm now doing work after executing a_func()")
+    return wrapTheFunction()
+
+
+def a_function_requiring_decoration():
+    print("I need a decoration")
+
+
+a_function_requiring_decoration()  # Outputs: "I need a decoration"
+print("Now let's wrap that string into decorator4 function\n")
+
+
+a_function_requiring_decoration = decorator4(a_function_requiring_decoration)
+# a_function_requiring_decoration is wrapped by WrapTheFunction()
+
+#a_function_requiring_decoration
+# outputs: I am doing som boring work before executing a_func()
+#          I am doing work after executing a_func()
+#          I am doing some boring work after executing a_func()
+
+
