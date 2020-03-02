28093803e36238ff21358a0edc186c8bc11107ef
Victorli888 <victorli7462@gmail.com>
2020-03-02 00:10:26 -0800

    Merge Sort doesn't work properly and quick sort needs comment review

--- Algorithms_Sort.py
+++ Algorithms_Sort.py
@@ -33,7 +33,64 @@ def merge(A, first, middle, last):  # list A, First, Middle, and Last index
 # else if the right element is smaller add it to where index [k] is in List A and increment the left index (j) over by 1
 
 
-list = [22, 34, 3, 55, 26, 2, 32, 78, 4, 8, 9]
+list = [5,9,1,2,10,4,8,6,3,7]
 print(f"Your current list looks like this: {list}")
 merge_sort(list)
+print(f"Your sorted list looks like this: {list}")
+print("\n\n\nExample 2: Quick Sort")
+# Quick sort is Recursive Dive and Conquer algorithm that is very efficient for large data sets.
+
+# SELECT A PIVOT NUMBER: We do this using the Median of three( By selecting a median number and picking the middle of
+# of the three.
+# From there we pick a Pivot point and put all the elements that are smaller to the left of the Pivot element.
+# and all the elements larger than the pivot number to the right of the pivot element.
+
+def quick_sort(A):
+    quick_sort2(A, 0, len(A) - 1)
+
+
+def quick_sort2(A, low, hi):
+    if low < hi:
+        p = partition(A, low, hi)
+        quick_sort2(A, low, p - 1)
+        quick_sort2(A, p + 1, hi)
+
+
+def get_pivot(A, low, hi):
+    mid = (hi + low) // 2
+    s = sorted([A[low], A[mid], A[hi]])
+    if s[1] == A[low]:
+        return low
+    elif s[1] == A[mid]:
+        return mid
+    return hi
+
+
+def partition(A, low, hi):
+    pivotIndex = get_pivot(A, low, hi)
+    pivotValue = A[pivotIndex]
+    A[pivotIndex], A[low] = A[low], A[pivotIndex]
+    border = low
+
+    for i in range(low, hi + 1):
+        if A[i] < pivotValue:
+            border += 1
+            A[i], A[border] = A[border], A[i]
+    A[low], A[border] = A[border], A[low]
+
+    return (border)
+
+
+def quick_selection(x, first, last):
+    for i in range(first, last):
+        minIndex = i
+        for j in range(i + 1, last + 1):
+            if x[j] < x[minIndex]:
+                minIndex = j
+        if minIndex != i:
+            x[i], x[minIndex] = x[minIndex], x[i]
+
+list = [5,9,1,2,4,8,6,3,7,10]
+print(f"Your current list looks like this: {list}")
+quick_sort(list)
 print(f"Your sorted list looks like this: {list}")
