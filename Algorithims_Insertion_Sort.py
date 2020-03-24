4eefa1a7cff4ab8661c68f26ffb56eedb844c8fd
Victorli888 <victorli7462@gmail.com>
2020-03-23 23:39:59 -0700

    Finished Insertion Sort

---
+++ Algorithims_Insertion_Sort.py
@@ -0,0 +1,34 @@
+# A list is only sorted if it is in ascending order. A descending order is considered the worst unsorted case.
+
+# For Insertion Sort you need to consider two parts of a list, an unsorted portion and a sorted portion
+# the sorted portion must stay sorted at all times, The first element is to be in the sorted portion and must be
+# compared to all the elements in the unsorted portion. Then INSERT  each unsorted element to its correct place.
+
+# Follow these rules
+
+# 1. Start with the first element and add it to the sorted portion of your list
+# 2. Compare the next element, If its bigger simply add it to the list and do nothing, else insert it to the left
+# 3. Restart
+# 4. Compare the next element, with the most recent element in the sorted list if bigger add it else move to the next
+# smallest element in the sorted list and repeat.
+# 5. Repeat until done.
+
+
+def insertionSort(alist):
+
+    for i in range(1, len(alist)):
+
+        # element to be compared
+        current = alist[i]
+
+        # compare the current element with the sorted portion and swapping
+        while i > 0 and alist[i-1] > current:
+            alist[i] = alist[i-1]
+            i = i-1
+            alist[i] = current
+
+        #print(alist)  # uncomment this part to see how the list actually gets sorted.
+    return alist
+
+list = [2,5,8,0,4,7,9,12,3,5,7]
+print(insertionSort(list))
