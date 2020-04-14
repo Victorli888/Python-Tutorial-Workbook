4f2b8eeebb213846ae751b47faecea816231b0b8
Victorli888 <victorli7462@gmail.com>
2020-04-13 23:30:47 -0700

    finished review, now its time to practice it

--- IC_Merged_storted_arrays.py
+++ IC_Merged_storted_arrays.py
@@ -14,6 +14,7 @@ Do we absolutely have to allocate a new list to use for the merged output? Where
 How would our function need to change?
 
  """
+import time
 
 def list_merger(lista, listb):
     # merge into one list
@@ -22,41 +23,33 @@ def list_merger(lista, listb):
     # list b has all values now we need to sort the values.
     return sorted(listb)
 
-
-lisa = [3,4,6,10,11,15]
-alice = [1,5,8,12,14,19]
-
-
-new_list = list_merger(lisa, alice)
-print(new_list)
-
 """
 space complexity: O(n)
 time complexity: O(n)
 """
 
 # Interview Cake solution
-def merge_lists(my_list, alices_list):
+def merge_lists(lisas_list, alices_list):
     # Set up our merged_list
-    merged_list_size = len(my_list) + len(alices_list)
+    merged_list_size = len(lisas_list) + len(alices_list)
     merged_list = [None] * merged_list_size
 
     current_index_alices = 0
-    current_index_mine = 0
+    current_index_lisa = 0
     current_index_merged = 0
     while current_index_merged < merged_list_size:
-        is_my_list_exhausted = current_index_mine >= len(my_list)
+        is_lisas_list_exhausted = current_index_lisa >= len(lisas_list)
         is_alices_list_exhausted = current_index_alices >= len(alices_list)
-        if (not is_my_list_exhausted and
+        if (not is_lisas_list_exhausted and
                 (is_alices_list_exhausted or
-                 my_list[current_index_mine] < alices_list[current_index_alices])):
+                 lisas_list[current_index_lisa] < alices_list[current_index_alices])):
             # Case: next comes from my list
-            # My list must not be exhausted, and EITHER:
+            # Lisa's must not be exhausted, and EITHER:
             # 1) Alice's list IS exhausted, or
             # 2) the current element in my list is less
             #    than the current element in Alice's list
-            merged_list[current_index_merged] = my_list[current_index_mine]
-            current_index_mine += 1
+            merged_list[current_index_merged] = lisas_list[current_index_lisa]
+            current_index_lisa += 1
         else:
             # Case: next comes from Alice's list
             merged_list[current_index_merged] = alices_list[current_index_alices]
@@ -65,3 +58,9 @@ def merge_lists(my_list, alices_list):
         current_index_merged += 1
 
     return merged_list
+
+lisa = [3,4,6,10,11,15]
+alice = [1,5,8,12,14,19]
+
+new_list = merge_lists(lisa, alice)
+print(new_list)
