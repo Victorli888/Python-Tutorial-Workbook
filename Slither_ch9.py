dd965f691106ac1ec1fea4a6655956c1d0e6f980
Victorli888 <victorli7462@gmail.com>
2020-05-23 22:46:26 -0700

    added review notes

---
+++ Slither_ch9.py
@@ -0,0 +1,6 @@
+"""
+Sorting Algorithm Review
+Please see Algorithms_bubble_sort Algorithms_insertion_sort etc, for more notes on sorting algorithms
+Please see Interview cake Review for more on Time Complexity and Space complexity of Sorting Algorithms
+
+"""
