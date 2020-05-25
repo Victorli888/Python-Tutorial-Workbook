a0201f432894901a472eeb27ca4fd89abc5166b5
Victorli888 <victorli7462@gmail.com>
2020-05-24 23:34:48 -0700

    finished Exercies 1 to 4

---
+++ Slither_ch10.py
@@ -0,0 +1,93 @@
+import sys
+
+
+"""
+Question 1: Write a program that multiplies an arbitrary number of command-line arguments together. 
+The arguments will be numbers
+
+your Program should run as follows:
+$ python3 calc.py 8 8 9 3 2
+3456
+
+"""
+
+
+# sys.argv is shorthand for argument vector, This is a list containing command line arguments passed to the script.
+# The arguments passed for the script come after the name of the script. i.e $ python3 Slither_ch10.py arg1 arg 2 ...
+
+
+def calc():
+
+    total = 1
+
+    i = 1
+    while i < len(sys.argv):
+        total *= int(sys.argv[i])
+        i += 1
+
+    print(total)
+
+
+# calc()
+
+"""
+Question 2: write a program that reads in lines from standard input (no redirection) and outputs them to 
+standard output. (No redirection)
+"""
+# standard input = stdin
+# read() - Reads the entire contents of the file and entire contents will be assigned to a single string
+# readlines() - stores each line as an element in a list
+# readline() - reads only  a single line from standard input, arguably the best because  it can save memory.
+
+
+def reader():
+
+    line = sys.stdin.readline()
+    while line:
+        print(line)
+        line = sys.stdin.readline()
+
+
+# reader()
+
+
+"""
+Question 3: Write a program that reads a text file and outputs how many words are contained within that file
+
+"""
+
+
+def word_count():
+    file_name = sys.argv[1]
+
+    with open(file_name, "r") as f:
+        text = f.read()
+
+    words = text.split()
+    print(len(words))
+
+
+# word_count()
+
+"""
+Question 4: Write a program that takes the contents of a file and determines whether or not a phrase is a palindrome.
+Words that are Palindromes should return True, else false.Capitals and spaces should not effect the answer.
+
+"""
+
+
+def palindrome():
+    file_name = sys.argv[1]
+
+    with open(file_name, "r") as f:
+        word = f.readline().strip()
+        while word:
+            processed = word.replace(" ", "").lower()
+            if processed == processed[::-1]:
+                print(True)
+            else:
+                print(False)
+            word = f.readline().strip()
+
+
+palindrome()
