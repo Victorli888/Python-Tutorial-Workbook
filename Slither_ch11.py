9e3b28d6e9bada547717a54c5747ce1ac4637968
Victorli888 <victorli7462@gmail.com>
2020-05-29 22:02:44 -0700

    started chapter 11

---
+++ Slither_ch11.py
@@ -0,0 +1,6 @@
+"""
+Question 1: Read a file that contains a shop's item stock. create a program that parses the file and builds a
+dictionary. Then output the available stock in alphabet order.
+"""
+
+def shop_stock():
