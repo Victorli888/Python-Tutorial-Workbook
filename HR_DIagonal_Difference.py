17920bcc7ed39b90a4a308bd4458b6288f840c32
Victorli888 <victorli7462@gmail.com>
2020-07-28 01:29:39 -0700

    finished brute force answer

---
+++ HR_DIagonal_Difference.py
@@ -0,0 +1,43 @@
+# Hacker Rank Question Link: https://www.hackerrank.com/challenges/diagonal-difference/problem
+"""
+Assumption: input is a 3x3 array
+"""
+
+# Brute force Method  Time O(n) Space: O(1)
+
+
+def diag_diff(arr):
+
+    lrd = 0  # left to right diagonal
+    rld = 0  # right to left diagonal
+    j = 0
+    i = 0
+
+    for item in arr:
+        lrd += arr[i][j]
+        i += 1
+        j += 1
+
+    j = 0
+    i = 2
+
+    for item in arr:
+        rld += arr[i][j]
+        j += 1
+        i -= 1
+    print(f"lrd={lrd}")
+    print(f"rld={rld}")
+
+    return abs(lrd-rld)
+
+
+test_1 = [[-1, 2, 3],
+         [4, -5, 6],
+         [7, 8, -9]]  # abs(-15 - 5b) = 0
+
+test_2 = [[10, 2, 3],
+         [4, 5, 6],
+         [7, 8, 9]]  # abs(24-15) = 9
+
+print(diag_diff(test_1))
+print(diag_diff(test_2))
