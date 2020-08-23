96d2109a544a2035839e6c7766832f7f396e5488
Victorli888 <victorli7462@gmail.com>
2020-08-22 23:48:03 -0700

    switch cases possible ans

---
+++ HR_GradingStudents.py
@@ -0,0 +1,14 @@
+"""
+Hacker Rank Question Link: https://www.hackerrank.com/challenges/grading/problem
+
+My solution:        Space Complexity:       Time Complexity:
+"""
+
+
+def gradingStudents(grades):
+
+
+
+#  Test input and output
+if __name__ == '__main__':
+    pass
