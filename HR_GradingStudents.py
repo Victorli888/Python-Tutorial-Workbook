09815e8a4a45bf16bee0b86d7c5dddaf1c6ba2af
Victorli888 <victorli7462@gmail.com>
2020-08-25 08:02:41 -0700

    reviewed code, and added comments

--- HR_GradingStudents.py
+++ HR_GradingStudents.py
@@ -1,5 +1,8 @@
 """
 Hacker Rank Question Link: https://www.hackerrank.com/challenges/grading/problem
+- if the difference of the next multiple of 5 is less than 3 then round that grade up to 5
+--- in other words 83 & 84 should round up to 85 and 88 & 89 should round up to 90
+- if the value is less than 38 then no rounding should occur because it is a failing grade
 
 My solution:        Space Complexity: O(1)      Time Complexity: O(n)
 """
@@ -20,3 +23,8 @@ if __name__ == '__main__':
     my_grades = [73, 67, 38, 33]  # expected is 75, 67, 40, 33
     curved_grades = grade_curve(my_grades)
     print(curved_grades)
+
+test1 = [71, 72, 73, 74, 75, 76, 77, 78, 79, 80]
+
+m = grade_curve(test1)
+print(m)
