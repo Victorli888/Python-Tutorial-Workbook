4f964569bf4e6ec68914f12016c7de4e6e528439
Victorli888 <victorli7462@gmail.com>
2020-08-24 21:58:40 -0700

    finished problem

--- HR_GradingStudents.py
+++ HR_GradingStudents.py
@@ -1,14 +1,22 @@
 """
 Hacker Rank Question Link: https://www.hackerrank.com/challenges/grading/problem
 
-My solution:        Space Complexity:       Time Complexity:
+My solution:        Space Complexity: O(1)      Time Complexity: O(n)
 """
 
 
-def gradingStudents(grades):
+def grade_curve(grades):  # input is an array
+    for i in range(len(grades)):
 
+        if grades[i] > 37:
+            if (grades[i] % 5) != 0:
+                if 5 - (grades[i] % 5) < 3:
+                    grades[i] += 5 - (grades[i] % 5)
+    return grades
 
 
 #  Test input and output
 if __name__ == '__main__':
-    pass
+    my_grades = [73, 67, 38, 33]  # expected is 75, 67, 40, 33
+    curved_grades = grade_curve(my_grades)
+    print(curved_grades)
