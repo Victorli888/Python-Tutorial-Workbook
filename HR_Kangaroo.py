adb85e88da21eec31db7001ec6c9bfaa685e5a64
Victorli888 <victorli7462@gmail.com>
2020-09-16 00:25:25 -0700

    finished problem

---
+++ HR_Kangaroo.py
@@ -0,0 +1,26 @@
+"""
+HackerRank Question Link: https://www.hackerrank.com/challenges/kangaroo/problem
+x1 - starting distance for Kangaroo 1
+v1 - jumping speed for Kangaroo 1
+x2 - starting distance for Kangaroo 2
+v2 - jumping speed for Kangaroo 2
+Find: whether or not both kangaroos eventually jump to the same spot
+Time Complexity: O(1)  Space Complexity: O(1)
+Worse case for time is O(10000) best case is O(1)
+"""
+def kangaroo(x1, v1, x2, v2):
+    j1 = 0
+    j2 = 0  # number of jumps by kangaroo 1 & 2
+    while True:
+        total1 = x1 + v1*j1
+        total2 = x2 + v2*j2
+        if total1 == total2 and j1 == j2:
+            return "YES"
+        elif j1 >= 10000:
+            return "NO"
+        j1 += 1
+        j2 += 1
+
+
+print(kangaroo(0,3,4,2)) # ans: YES
+print(kangaroo(0,2,5,3)) # ans: NO
