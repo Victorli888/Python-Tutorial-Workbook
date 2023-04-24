430b4f5fda2b0c050160fb0ff1a0ec22165576fb
victorli <victorli7462@gmail.com>
2023-04-24 07:53:13 -0700

    rename variables for clarity

--- Algorithms/InsertionSort.py
+++ Algorithms/InsertionSort.py
@@ -1,34 +1,28 @@
-# A list is only sorted if it is in ascending order. A descending order is considered the worst unsorted case.
+"""
+https://www.youtube.com/watch?v=8mJ-OhcfpYg - Good Visual & Explanation on how Insertion sort wroks
 
-# For Insertion Sort you need to consider two parts of a list, an unsorted portion and a sorted portion
-# the sorted portion must stay sorted at all times, The first element is to be in the sorted portion and must be
-# compared to all the elements in the unsorted portion. Then INSERT  each unsorted element to its correct place.
-
-# Follow these rules
-
-# 1. Start with the first element and add it to the sorted portion of your list
-# 2. Compare the next element, If its bigger simply add it to the list and do nothing, else insert it to the left
-# 3. Restart
-# 4. Compare the next element, with the most recent element in the sorted list if bigger add it else move to the next
-# smallest element in the sorted list and repeat.
-# 5. Repeat until done.
+Summary:
+have a temp variable to store element in our given array, and compare whether temp is smaller, when our temp is larger
+than the element to the next left OR if the end is reached INSERT temp at that position.
+"""
 
 
-def insertionSort(alist):
+def insertion_sort(nums):
 
-    for i in range(0, len(alist)):
+    # idx represents the current pointer
+    for idx in range(0, len(nums)):
 
-        # element to be compared
-        current = alist[i]
+        # hold current element for comparison
+        temp = nums[idx]
 
-        # compare the current element with the sorted portion and swapping
-        while i > 0 and alist[i-1] > current:
-            alist[i] = alist[i-1]
-            i = i-1
-            alist[i] = current
+        # if the left element is smaller than our current, we need to swap and shift the pointer.
+        while idx > 0 and nums[idx - 1] > temp:
+            nums[idx] = nums[idx - 1]
+            idx -= 1
+            nums[idx] = temp
 
-        #print(alist)  # uncomment this part to see how the list actually gets sorted.
-    return alist
+    return nums
+
 
 def recursive(arr, n):
     if n > len(arr):
@@ -41,11 +35,3 @@ def recursive(arr, n):
         curr -= 1
     n += 1
     return arr
-
-
-
-
-
-
-list = [2,5,8,0,4,7,9,12,3,5,7]
-print(recursive(list, 0))
