aaeaeab6ec684bdf9e293d84e6d88fad538f3711
victorli <victorli7462@gmail.com>
2023-04-24 09:01:09 -0700

    added Docu string for space and time analysis

--- Algorithms/InsertionSort.py
+++ Algorithms/InsertionSort.py
@@ -8,14 +8,20 @@ than the element to the next left OR if the end is reached INSERT temp at that p
 
 
 def insertion_sort(nums):
-
-    # idx represents the current pointer
+    """
+    Notes: we follow our element through the list until we find the appropriate spot before moving onto the next idx
+    Time: Average Case:O(n^2) --- Best case: O(n) (when nums is already sorted we don't have to use while loop logic.)
+    Space: O(1) - Sorts in place.
+    :param nums: Given array of integers
+    :return:
+    """
+    # starts at 0, but after looking closely at the nested while loop, logic doesn't apply until idx = 2
     for idx in range(0, len(nums)):
 
         # hold current element for comparison
         temp = nums[idx]
 
-        # if the left element is smaller than our current, we need to swap and shift the pointer.
+        # if the left element is smaller than our current temp, swap and decrement our pointer to follow current temp
         while idx > 0 and nums[idx - 1] > temp:
             nums[idx] = nums[idx - 1]
             idx -= 1
