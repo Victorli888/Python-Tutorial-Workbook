0d25a89e597fbc5413a9165160beb4a0cf21c70e
victorli <victorli7462@gmail.com>
2023-05-15 07:25:19 -0700

    recursive vs itterative

--- LeetCode/FibonacciNumber509.py
+++ LeetCode/FibonacciNumber509.py
@@ -2,11 +2,12 @@
 # Find the Fibonacci Number at Nth term
 # 0,1,1,2,3,5,8,13,21
 # need first two terms in order to calculate Fib sequence
-def findfib(n):
+def find_next_number(n):
     """
     O(n) Time
     O(1) Space
-    :param n: Nth number in fib sequence
+    find_next_number(4) = 3
+    :param n: next number after Nth number in fib sequence
     :return: Number at Nth position
     """
     if n == 0:
@@ -22,4 +23,31 @@ def findfib(n):
         term_1 = term_2
         term_2 = next_number
     return next_number
+
+def find_sequence(n):
+    """
+    Find Sequence for up to nth term
+    :param n: determine how many numbers in the sequence should be produced
+    :return: fibonacci sequence as a list
+    """
+    if n == 0:
+        return []
+    if n == 1:
+        return [0]
+    if n == 2:
+        return[0, 1]
+
+    sequence = [0, 1]
+    for i in range(2, n):
+        next_number = sequence[i-1] + sequence[i-2]
+        sequence.append(next_number)
+    return sequence
+
+def find_next_recursively(n):
+    if n == 0:
+        return 0
+    if n == 1:
+         return 1
+
+    return find_next_recursively(n-1) + find_next_recursively(n-2)
 
